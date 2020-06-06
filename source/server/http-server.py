10fe14cbecdcdf7287a00ffd25f70cdefa182b00
bcanozter <burak.ozter@dal.ca>
2020-06-06 04:41:23 -0300

    npm i axios

--- source/server/http-server.py
+++ source/server/http-server.py
@@ -10,6 +10,5 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b'<html><head><title>Title goes here.</title></head>')
 
-
 httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
 httpd.serve_forever()
